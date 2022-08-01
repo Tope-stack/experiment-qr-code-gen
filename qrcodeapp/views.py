af296c3fa1b9eb64ead828f5b82762ed5a7386e9
Tope-stack <yusufftope70@gmail.com>
2022-08-01 15:02:03 +0100

    Generate qr code not working

---
+++ qrcodeapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.conf import settings
+from qrcode import *
+import time 
+
+# Create your views here.
+def qr_gen(request):
+    if request.method == 'POST':
+        data = request.POST['data']
+        img = make(data)
+        img_name = 'qr' + str(time.time()) + '.png'
+        img.save(settings.MEDIA_ROOT + '/' + img_name)
+        return render(request, 'index.html', {'img_name': img_name})
+    return render(request, 'index.html')
