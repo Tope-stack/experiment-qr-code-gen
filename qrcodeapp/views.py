b54abcae408508139b7be2980d63053f8e838d1c
Tope-stack <yusufftope70@gmail.com>
2022-08-01 16:04:01 +0100

    Qr gen now working

--- qrcodeapp/views.py
+++ qrcodeapp/views.py
@@ -9,6 +9,7 @@ def qr_gen(request):
         data = request.POST['data']
         img = make(data)
         img_name = 'qr' + str(time.time()) + '.png'
-        img.save(settings.MEDIA_ROOT + '/' + img_name)
+        img.save(str(settings.MEDIA_ROOT) + '/' + img_name)
+        # img.save(str(settings.MEDIA_ROOT / '/' + img_name)
         return render(request, 'index.html', {'img_name': img_name})
     return render(request, 'index.html')
